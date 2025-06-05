8ff7e821e270af7ee611dfa4a6de65db096630c4
hannavazhlivtseva <anna.vazhlivtseva@gmail.com>
2025-06-05 13:59:36 +0300

    csrf_token

--- views/pageB.php
+++ views/pageB.php
@@ -1,6 +1,7 @@
 <h2>Page B: Download</h2>
 
 <form action="/download" method="post">
+    <input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars($csrf_token) ?>">
     <button type="submit" class="btn btn-success">Download</button>
 </form>
 
